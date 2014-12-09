a5cd6380e413353bf4e42c79263b1f5d756dd828
Brian Newton <brian.newton@lyncstream.com>
2014-12-09 00:10:48 -0600

    Adding lodash, gulp, and all gulp-* plugins as global variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,6 @@
+global._            = require('lodash');
+global.gulp         = require('gulp');
+global.plugins      = require('gulp-load-plugins')();
+
 require('coffee-script/register');
 require('require-dir')('./gulp');
