7e88e01126782db5112bb8c6e7d24364eb8c505a
Brian Newton <brian.newton@lyncstream.com>
2016-10-05 21:59:24 -0500

    Adding disabled and pointer cursor styles for the arrows

--- src/js/images/index.js
+++ src/js/images/index.js
@@ -19,12 +19,17 @@ module.exports = {
 
     vm.page = 1;
     vm.imagesPerPage = 5
+    vm.canPageLeft = function(){
+        return vm.page > 1;
+    };
     vm.pageLeft = function(){
         if(vm.page > 1){
           vm.page -= 1;
         }
     };
-
+    vm.canPageRight = function(){
+        return vm.page < Math.ceil(vm.images.length / vm.imagesPerPage);
+    };
     vm.pageRight = function(){
       if(vm.page < Math.ceil(vm.images.length / vm.imagesPerPage)){
         vm.page += 1;
@@ -35,17 +40,17 @@ module.exports = {
   view: function imagesView(ctrl){
     return m('.images-page', [
       m('h1.page-title', ctrl.imageType()),
-      m('.pure-g', [
+      m('.pure-g', [ // TODO: add arrows to the selected image too
         m('.pure-u-5-5.selected-image', m('img.pure-img', { src: ctrl.selectedImage() })),
         m('.pure-u-5-5',
           m('.pure-g.images', [
-            m('.arrow.pure-u-1-12', { onclick: ctrl.pageLeft },  '<'),
+            m('.arrow.pure-u-1-12', { "class": ctrl.canPageLeft() ? "" : "disabled", onclick: ctrl.pageLeft },  '<'),
             m('.pure-u-5-6', m('.pure-g',
               ctrl.images.slice((ctrl.page - 1) * 5, (ctrl.page * 5)).map(function(image){
                 return m('.pure-u-1-5.image-container', { onclick: function(){ ctrl.selectedImage(image); } }, m('img.pure-img', { src: image }));
               }))
             ),
-            m('.arrow.pure-u-1-12', { onclick: ctrl.pageRight }, '>')
+            m('.arrow.pure-u-1-12', { "class": ctrl.canPageRight() ? "" : "disabled", onclick: ctrl.pageRight }, '>')
           ])
         )
       ])
