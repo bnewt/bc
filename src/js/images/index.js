c360e50a4b0f834a601709fff33a0840472f3856
Brian Newton <brian.newton@lyncstream.com>
2016-10-07 19:00:46 -0500

    Make move left and right buttons for the main image work correctly

--- src/js/images/index.js
+++ src/js/images/index.js
@@ -12,13 +12,33 @@ module.exports = {
       return m.route('/home');
     }
 
-
     vm.imageType = m.prop(type);
     vm.images = imageService.getImagesOfType(type);
     vm.selectedImage = m.prop(vm.images[0]);
 
     vm.page = 1;
     vm.imagesPerPage = 5
+
+    vm.index = function(){
+      return indexOf(vm.images, vm.selectedImage());
+    };
+    vm.canMoveLeft = function(){
+      return vm.index() > 0;
+    };
+    vm.moveLeft = function(){
+        if(vm.canMoveLeft()){
+          vm.selectedImage(vm.images[vm.index() - 1]);
+        }
+    };
+    vm.canMoveRight = function(){
+        return vm.index() + 1 < vm.images.length;
+    };
+    vm.moveRight = function(){
+      if(vm.canMoveRight()){
+        vm.selectedImage(vm.images[vm.index() + 1]);
+      }
+    };
+
     vm.canPageLeft = function(){
         return vm.page > 1;
     };
@@ -35,16 +55,15 @@ module.exports = {
         vm.page += 1;
       }
     };
-
   },
   view: function imagesView(ctrl){
     return m('.images-page', [
       m('h1.page-title', ctrl.imageType()),
       m('.pure-g', [ // TODO: add arrows to the selected image too
         m('.pure-u-5-5', m("pure-g", [
-          m('.arrow.pure-u-1-12', { "class": ctrl.canPageLeft() ? "" : "disabled", onclick: ctrl.pageLeft },  '<'),
+          m('.arrow.pure-u-1-12', { "class": ctrl.canMoveLeft() ? "" : "disabled", onclick: ctrl.moveLeft },  '<'),
           m('.pure-u-5-6.selected-image', m('img.pure-img', { src: ctrl.selectedImage() })),
-          m('.arrow.pure-u-1-12', { "class": ctrl.canPageRight() ? "" : "disabled", onclick: ctrl.pageRight }, '>')
+          m('.arrow.pure-u-1-12', { "class": ctrl.canMoveRight() ? "" : "disabled", onclick: ctrl.moveRight }, '>')
         ])),
         m('.pure-u-5-5',
           m('.pure-g.images', [
