bcf8fb87bf4b7cfa57f2407e49f678b5cdbb9542
Brian Newton <brian.newton@lyncstream.com>
2016-12-04 23:14:39 -0600

    Update homepage on smaller devices

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -7,17 +7,17 @@ module.exports = {
          m('.pure-g',
             m('img.pure-u-5-5[src="img/2015_00283-cropped.JPG"]')),
          m('h1.home-centered-text', '"DESIGN TO FINISH"'),
-         m('.h3.home-centered-text', 'INTERIOR/EXTERIOR, KITCHENS, BATHROOMS,'),
-         m('.h3.home-centered-text', 'ROOM ADDITIONS, LOWER LEVEL FINISHES, DECKS'),
+         m('.h3.home-centered-text.home-remodel-types', 'INTERIOR/EXTERIOR, KITCHENS, BATHROOMS,'),
+         m('.h3.home-centered-text.home-remodel-types', 'ROOM ADDITIONS, LOWER LEVEL FINISHES, DECKS'),
          m('.pure-g', [
-           m('ul.pure-u-1-3', [
+           m('ul.pure-u-1-3.home-bullet-points', [
              m('li', 'MEMBER OF BBB'),
              m('li', 'FULL DESIGN SERVICES'),
              m('li', m.trust('ALL WORKERS FULLY LICENSED &amp; INSURED')),
              m('li', 'YOU CAN TRUST US!'),
              m('li', '2 YEAR WRITTEN GUARANTEE')
            ]),
-           m('ul.pure-u-1-2', [
+           m('ul.pure-u-1-2.home-bullet-points', [
              m('li', m.trust('JOB SITE CLEANLINESS &amp; CLEANUP')),
              m('li', m.trust('PROTECTING FLOORING &amp; FINISHES')),
              m('li', 'WE WORK DIRECTLY WITH YOU FROM START TO FINISH!'),
